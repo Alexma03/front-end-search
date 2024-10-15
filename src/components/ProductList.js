fd0c3c3e24d75dfe0f63d1daaa70efb3ad576119
Alejandro Marcos <alex03marcos@gmail.com>
2024-10-15 10:52:03 +0200

    feature delete product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,7 @@
 export function renderProducts(products) {
   const productList = document.getElementById("productList");
   productList.innerHTML = "";
+
   products.forEach((product) => {
     const productCard = document.createElement("div");
     productCard.className = "product-card";
@@ -13,6 +14,26 @@ export function renderProducts(products) {
           <p class="description">${product.descripcion}</p>
         </div> 
       `;
+
+    productCard.addEventListener("click", async () => {
+      try {
+        const response = await fetch(
+          `https://api-search-products-d92dad668ba2.herokuapp.com/products/delete-product/${product.id}`,
+          {
+            method: "DELETE",
+          }
+        );
+
+        if (response.ok) {
+          productCard.remove();
+        } else {
+          console.error("Error al eliminar el producto:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error al eliminar el producto:", error);
+      }
+    });
+
     productList.appendChild(productCard);
   });
 }
